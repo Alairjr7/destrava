06f6ce8af76e847fd8a2e732498903f0a23e5a98
Alair Junior <149719865+Alairjr7@users.noreply.github.com>
2026-08-03 19:25:56 -0300

    feat: criando a seção principal da landing page com prévia do produto

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import { Header } from "./components/landing/Header";
+import { Hero } from "./components/landing/Hero";
 
 export function App() {
   return (
     <>
       <Header />
       <main>
-        
+        <Hero />
       </main>
     </>
   );
